51ddbf1c1e8107cb8955abd05bbc828b5caecc5d
Wagner Alves <guinn83@hotmail.com>
2022-09-05 16:22:39 -0300

    Project clean

--- backend/src/main/java/com/wa/dsmeta/DsmetaApplication.java
+++ backend/src/main/java/com/wa/dsmeta/DsmetaApplication.java
@@ -1,13 +1,13 @@
-package com.wa.backend;
+package com.wa.dsmeta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class DsmetaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(DsmetaApplication.class, args);
 	}
 
 }
